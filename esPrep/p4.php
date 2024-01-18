e7ad578834f4f397672c8a6e4411a308b2898d08
th3-7rue <riccardorasori2005@gmail.com>
2024-01-18 20:23:30 +0100

    ok

--- esPrep/p4.php
+++ esPrep/p4.php
@@ -9,20 +9,26 @@ session_start();
 require 'conn.php';
 if (isset($_GET['film'])) {
     $film = $_GET['film'] .= '%';
-    $stmt = $mysqli->prepare('delete from recita join film on recita.fkFilm=film.id where film.titolo like ?');
+    $stmt = $mysqli->prepare('delete recita from recita join film on recita.fkFilm=film.id where film.titolo like ?');
     $stmt->bind_param('s', $film);
     $stmt->execute();
-    $rows = $mysqli->affected_rows;
+    $stmt->prepare('delete appartiene from appartiene join film on appartiene.fkFilm=film.id where film.titolo like ?');
+    $stmt->bind_param('s', $film);
+    $stmt->execute();
+    $stmt->prepare('delete from film where titolo like ?');
+    $stmt->bind_param('s', $film);
+    $stmt->execute();
+    $rows = $stmt->affected_rows;
     if ($rows = 1) {
-        ?>
+?>
         <div>E' stato eliminato un film</div>
-        <?php
+    <?php
 
     } else {
-        ?>
+    ?>
         <div>Sono stati eliminati
             <?php $rows ?> film
         </div>
-        <?php
+<?php
     }
 }
